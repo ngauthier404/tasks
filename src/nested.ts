05c5eef188b26479d2c1fbf6aa3212338928d4d0
Nicholas Gauthier <ngauthier@mockingjaylaptop.local>
2023-10-02 16:12:07 -0400

    Coming back to this, finishing the last function

--- src/nested.ts
+++ src/nested.ts
@@ -1,6 +1,6 @@
 import { Answer } from "./interfaces/answer";
 import { Question, QuestionType } from "./interfaces/question";
-import { makeBlankQuestion } from "./objects";
+import { duplicateQuestion, makeBlankQuestion } from "./objects";
 
 /**
  * Consumes an array of questions and returns a new array with only the questions
@@ -273,5 +273,20 @@ export function duplicateQuestionInArray(
     targetId: number,
     newId: number
 ): Question[] {
-    return [];
+    return questions
+        .map((question: Question): Question[] =>
+            question.id == targetId
+                ? [
+                      { ...question, options: [...question.options] },
+                      duplicateQuestion(newId, question)
+                  ]
+                : [{ ...question, options: [...question.options] }]
+        )
+        .reduce(
+            (
+                previousValue: Question[],
+                currentValue: Question[]
+            ): Question[] => [...previousValue, ...currentValue],
+            []
+        );
 }
