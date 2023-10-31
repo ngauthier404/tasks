247abc929ab6121bf5240695e2cb9aad7a82fe92
Nick Gauthier <ngauthier404@icloud.com>
2023-10-31 16:36:39 -0400

    Forms tasks

--- src/form-components/ChangeColor.tsx
+++ src/form-components/ChangeColor.tsx
@@ -1,9 +1,47 @@
 import React, { useState } from "react";
+import { Col, Form, Row } from "react-bootstrap";
+
+const colors: string[] = [
+    "red",
+    "blue",
+    "green",
+    "orange",
+    "purple",
+    "cyan",
+    "pink",
+    "white"
+];
 
 export function ChangeColor(): JSX.Element {
+    const [color, setColor] = useState<string>(colors[0]);
+
     return (
         <div>
             <h3>Change Color</h3>
+            <Form.Group controlId="changeColor" as={Row}>
+                {colors.map((option: string) => (
+                    <Col key={option}>
+                        <Form.Check
+                            inline
+                            type="radio"
+                            name="color"
+                            value={option}
+                            onChange={(e) => setColor(e.target.value)}
+                            id={option}
+                        />
+                        <div style={{ backgroundColor: option }}>{option}</div>
+                    </Col>
+                ))}
+            </Form.Group>
+            <div>
+                You have chosen
+                <div
+                    data-testid="colored-box"
+                    style={{ backgroundColor: color }}
+                >
+                    {color}
+                </div>
+            </div>
         </div>
     );
 }
--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,9 +1,35 @@
 import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
 
 export function GiveAttempts(): JSX.Element {
+    const [attempts, setAttempts] = useState<number>(3);
+    const [request, setRequest] = useState<string>("");
+
     return (
         <div>
             <h3>Give Attempts</h3>
+            <h6>Attempts left: {attempts}</h6>
+            <Form.Control
+                type="number"
+                value={request}
+                onChange={(e) => setRequest(e.target.value)}
+            />
+            <Button
+                disabled={attempts == 0}
+                onClick={() => setAttempts(attempts - 1)}
+            >
+                Use
+            </Button>
+            <Button
+                onClick={() => {
+                    const x = Number(request);
+                    if (!isNaN(x)) {
+                        setAttempts(attempts + x);
+                    }
+                }}
+            >
+                Gain
+            </Button>
         </div>
     );
 }
